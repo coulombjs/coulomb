9d4bf58890a6c3945e2e0a44b468c62de484ea4c
Anton Strogonoff <anton@strogonoff.name>
2020-02-02 19:23:16 +0800

    refactor: MainApp: Use generic Backend instead of Versioned

--- src/app/main.ts
+++ src/app/main.ts
@@ -10,7 +10,7 @@ import { MainConfig } from '../config/main';
 import { SettingManager } from '../settings/main';
 import { notifyAllWindows } from '../main/window';
 import {
-  VersionedFilesystemBackend,
+  Backend,
   VersionedManager,
   BackendClass as DatabaseBackendClass,
 } from '../db/main/base';
@@ -278,6 +278,6 @@ export interface MainApp<A extends AppConfig, M extends MainConfig<A>> {
   isMacOS: boolean
   isDevelopment: boolean
   managers: Record<keyof A["data"], VersionedManager<any, any>>
-  databases: Record<keyof M["databases"], VersionedFilesystemBackend>
+  databases: Record<keyof M["databases"], Backend>
   openWindow: (windowName: keyof A["windows"]) => void
 }
