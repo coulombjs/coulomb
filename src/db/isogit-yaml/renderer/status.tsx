0b3f1c4e3d20aa62b68efb7c8d2b3b3e90d458de
Anton Strogonoff <anton@strogonoff.name>
2020-02-10 14:26:40 +0800

    feat: Major IsoGit status widget update

--- src/db/isogit-yaml/renderer/status.tsx
+++ src/db/isogit-yaml/renderer/status.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-import { Button, IconName, Tooltip, FormGroup, InputGroup, Intent, Icon, Popover, Position } from '@blueprintjs/core';
+import { Button, IconName, Tooltip, FormGroup, InputGroup, Intent, Text, Popover, Position } from '@blueprintjs/core';
 
 import { openWindow } from '../../../api_legacy/renderer';
 import { callIPC, useIPCValue } from '../../../ipc/renderer';
@@ -13,41 +13,69 @@ import styles from './status.scss';
 
 const BackendDetails: React.FC<DatabaseStatusComponentProps<BackendDescription, BackendStatus>> =
 function ({ dbIPCPrefix, status, description }) {
-  return (
-    <div className={styles.base}>
-      <div className={styles.sourceInfo}>
-        {description.gitUsername}@{description.gitRepo}
-      </div>
-      <Status ipcPrefix={dbIPCPrefix} status={status} />
-    </div>
-  );
-};
-
-
-export default BackendDetails;
-
-
-const Status: React.FC<{ ipcPrefix: string, status: BackendStatus }> = function ({ ipcPrefix, status }) {
-  const numUncommitted = useIPCValue(`${ipcPrefix}-count-uncommitted`, { numUncommitted: 0 }).
-  value.numUncommitted;
-
-  const [passwordPromptIsOpen, openPasswordPrompt] = useState(false);
+  const ipcPrefix = dbIPCPrefix;
 
   useEffect(() => {
     openPasswordPrompt(status.needsPassword);
   }, [status.needsPassword]);
 
-  async function triggerSync() {
-    await callIPC(`${ipcPrefix}-git-trigger-sync`);
-  }
-
-  async function discardUnstaged() {
-    await callIPC(`${ipcPrefix}-git-discard-unstaged`);
-  }
+  const [passwordPromptIsOpen, openPasswordPrompt] = useState(false);
 
   async function setPassword(password: string) {
     await callIPC<{ password: string }, { success: true }>(`${ipcPrefix}-git-set-password`, { password });
   }
+
+  return (
+    <div className={styles.base}>
+      <Button
+          minimal={true}
+          small={true}
+          className={styles.sourceInfo}
+          onClick={() => callIPC('open-arbitrary-window', {
+            url: description.gitRepo,
+            title: "Git repository"
+          })}>
+        {description.gitUsername}@{description.gitRepo}
+      </Button>
+
+      <Popover minimal={true} content={
+          <PasswordPrompt
+            onConfirm={async (password) => { await setPassword(password); openPasswordPrompt(false); }} />}
+            position={Position.BOTTOM_LEFT}
+            isOpen={passwordPromptIsOpen}>
+        <ActionableStatus
+          status={status}
+          uncommittedFileCount={
+            useIPCValue(`${ipcPrefix}-count-uncommitted`, { numUncommitted: 0 }).
+            value.numUncommitted}
+          onRequestSync={async () => await callIPC(`${ipcPrefix}-git-trigger-sync`)}
+          onDiscardUnstaged={async () => await callIPC(`${ipcPrefix}-git-discard-unstaged`)}
+          onTogglePasswordPrompt={() => openPasswordPrompt(!passwordPromptIsOpen)}
+          onShowCommitWindow={() => openWindow('batch-commit')}
+          onShowSettingsWindow={() => openWindow('settings')}
+        />
+      </Popover>
+    </div>
+  );
+};
+
+export default BackendDetails;
+
+
+interface ActionableStatusProps {
+  status: BackendStatus
+  uncommittedFileCount: number
+  onRequestSync: () => Promise<void>
+  onDiscardUnstaged: () => Promise<void>
+  onTogglePasswordPrompt: () => void
+  onShowCommitWindow: () => void
+  onShowSettingsWindow: () => void
+}
+const ActionableStatus: React.FC<ActionableStatusProps> = function ({
+    status, uncommittedFileCount,
+    onRequestSync, onDiscardUnstaged,
+    onTogglePasswordPrompt,
+    onShowCommitWindow, onShowSettingsWindow }) {
 
   let statusIcon: IconName;
   let tooltipText: string | undefined;
@@ -56,93 +84,80 @@ const Status: React.FC<{ ipcPrefix: string, status: BackendStatus }> = function 
 
   if (status.isMisconfigured) {
     statusIcon = "error";
-    tooltipText = "Remote storage is missing configuration";
+    tooltipText = "Configuration required; click to resolve";
     statusIntent = "danger";
-    action = () => openWindow('settings');
+    action = onShowSettingsWindow;
 
   } else if (status.needsPassword) {
     statusIcon = "lock";
-    tooltipText = "Remote storage is pending authentication";
+    tooltipText = "Password required";
     statusIntent = "primary";
-    action = null;
+    action = onTogglePasswordPrompt;
 
   } else if (status.isOnline !== true) {
     statusIcon = "offline";
-    tooltipText = "No connection to remote storage";
+    tooltipText = "Offline"
     statusIntent = "danger";
-    action = triggerSync;
+    action = onRequestSync;
 
   } else if (status.hasLocalChanges) {
     statusIcon = "git-commit";
-    tooltipText = "Uncommitted local changes present—click to resolve";
+    tooltipText = "Uncommitted changes; click to resolve";
     statusIntent = "warning";
     action = async () => {
-      // If hasLocalChanges says yes, but uncommitted file count says no,
-      // try to fix it.
-      if (status.hasLocalChanges && numUncommitted < 1) {
-        await discardUnstaged();
-        await triggerSync();
+      if (status.hasLocalChanges && uncommittedFileCount < 1) {
+        // NOTE: If hasLocalChanges says yes, but uncommitted file count says no, try to fix it.
+        await onDiscardUnstaged();
+        await onRequestSync();
       } else {
-        openWindow('batch-commit');
+        onShowCommitWindow();
       }
     }
 
   } else if (status.isPulling) {
     statusIcon = "cloud-download"
-    tooltipText = "Synchronizing remote storage…";
+    tooltipText = "Synchronizing";
     statusIntent = "primary";
     action = null;
 
   } else if (status.isPushing) {
     statusIcon = "cloud-upload"
-    tooltipText = "Synchronizing remote storage…";
+    tooltipText = "Synchronizing";
     statusIntent = "primary";
     action = null;
 
   } else if (status.statusRelativeToLocal === 'diverged') {
     statusIcon = "git-branch"
-    tooltipText = "Local and remote storage have diverging changes—click to retry";
+    tooltipText = "Diverging changes present; click to retry";
     statusIntent = "danger";
-    action = triggerSync;
+    action = onRequestSync;
 
   } else if (status.statusRelativeToLocal === 'behind') {
     statusIcon = "cloud-upload"
-    tooltipText = "Pending changes to upload";
+    tooltipText = "Online";
     statusIntent = "warning";
-    action = null;
+    action = onRequestSync;
 
   } else {
     statusIcon = "updated"
-    tooltipText = "Click to trigger remote storage sync";
+    tooltipText = "Online";
     statusIntent = "success";
-    action = triggerSync;
+    action = onRequestSync;
   }
 
-  return <>
-    <Popover minimal={true} content={
-        <PasswordPrompt
-          onConfirm={async (password) => { await setPassword(password); openPasswordPrompt(false); }} />}
-          position={Position.BOTTOM_LEFT}
-          isOpen={passwordPromptIsOpen}>
-      <div className={styles.backendStatus}>
-        {action !== null
-          ? <Button
-              className={styles.statusIcon}
-              icon={statusIcon}
-              large={true}
-              onClick={action}
-              intent={statusIntent} />
-          : <Icon
-              icon={statusIcon}
-              intent={statusIntent}
-              className={styles.statusIcon}
-              iconSize={Icon.SIZE_LARGE} />}
-        <div className={styles.statusText}>
-          {tooltipText}
-        </div>
-      </div>
-    </Popover>
-  </>;
+  return (
+    <Button
+        className={styles.backendStatus}
+        onClick={action || (() => {})}
+        small={true}
+        minimal={true}
+        icon={statusIcon}
+        intent={statusIntent}
+        disabled={action === null}
+        loading={action === null}>
+      {tooltipText}
+    </Button>
+  );
 };
 
 
