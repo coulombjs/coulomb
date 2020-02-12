d548c52e381fd23c7633bbce13955b44491264f3
Anton Strogonoff <anton@strogonoff.name>
2020-02-12 21:52:01 +0800

    feat: Make renderApp() sync, implement useData hook

--- src/app/renderer.tsx
+++ src/app/renderer.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import * as log from 'electron-log';
 import * as ReactDOM from 'react-dom';
+
 import { AppConfig } from '../config/app';
 import { RendererConfig } from '../config/renderer';
+
+import { Model } from '../db/models';
+import { Index } from '../db/query';
 
 import { NonIdealState, Spinner } from '@blueprintjs/core';
 
@@ -10,17 +14,26 @@ import '!style-loader!css-loader!@blueprintjs/datetime/lib/css/blueprint-datetim
 import '!style-loader!css-loader!@blueprintjs/core/lib/css/blueprint.css';
 import '!style-loader!css-loader!./normalize.css';
 import '!style-loader!css-loader!./renderer.css';
+import { useIPCEvent, useIPCValue } from '../ipc/renderer';
 
 
-interface AppRenderer {
-  root: HTMLElement,
+interface UseDataHookResult<M extends Model> {
+  objects: Index<M>
+}
+
+type UseDataHook<C extends RendererConfig<any>> =
+<M extends Model, Q extends object>(modelName: keyof C["app"]["data"], query: Q) => UseDataHookResult<M>
+
+interface AppRenderer<C extends RendererConfig<any>> {
+  root: HTMLElement
+  useData: UseDataHook<C>
 }
 
 
 // Render application screen in a new window
 // with given top-level window UI component and (if applicable) any parameters
 // wrapped in configured context provider components.
-export const renderApp = async <A extends AppConfig, C extends RendererConfig<A>>(config: C): Promise<AppRenderer> => {
+export const renderApp = <A extends AppConfig, C extends RendererConfig<A>>(config: C): AppRenderer<C> => {
 
   // electron-webpack guarantees presence of #app in index.html it bundles
   const appRoot = document.getElementById('app') as HTMLElement;
@@ -37,63 +50,90 @@ export const renderApp = async <A extends AppConfig, C extends RendererConfig<A>
 
   log.debug(`Requested window component ${componentId}`);
 
+  const useData: UseDataHook<C> =
+  <M extends Model, Q extends object = any>
+  (modelName: keyof A["data"], query?: Q) => {
+    /* Queries data for specified model, listens for update events and updates the dataset. */
+
+    const objects = useIPCValue<Q, Index<M>>(`model-${modelName}-read-all`, {}, query);
+
+    useIPCEvent<{ ids?: string[] }>(`model-${modelName}-objects-changed`, function ({ ids }) {
+      const trackedObjectIDs = Object.keys(objects);
+      const shouldRefresh = ids !== undefined
+        ? ids.filter(id => trackedObjectIDs.includes(id)).length > 0
+        : false;
+      if (shouldRefresh) {
+        objects.refresh();
+      }
+    });
+
+    return { objects: objects.value };
+  }
+
   // Fetch top-level UI component class and render it.
   if (componentImporter) {
-    // Show loading indicator while components are being resolved
-    ReactDOM.render(<Spinner />, appRoot);
+    (async () => {
+      // Show loading indicator while components are being resolved
+      ReactDOM.render(<Spinner />, appRoot);
 
-    // Get props prescribed for each context provider component
-    var ctxProviderProps = config.contextProviders.map(item => item.getProps(config));
+      // Get props prescribed for each context provider component
+      var ctxProviderProps = config.contextProviders.map(item => item.getProps(config));
 
-    log.silly(
-      `C/renderApp: Resolving components`,
-      componentImporter, config.contextProviders);
+      log.silly(
+        `C/renderApp: Resolving components`,
+        componentImporter, config.contextProviders);
 
-    // Resolve (import) components in parallel, first UI and then context providers
-    const promisedComponents: { default: React.FC<any> }[] = await Promise.all([
-      componentImporter(),
-      ...config.contextProviders.map(async (ctxp) => await ctxp.cls()),
-    ]);
+      // Resolve (import) components in parallel, first UI and then context providers
+      const promisedComponents: { default: React.FC<any> }[] = await Promise.all([
+        componentImporter(),
+        ...config.contextProviders.map(async (ctxp) => await ctxp.cls()),
+      ]);
 
-    log.silly(
-      `C/renderApp: Resolved components`,
-      promisedComponents);
+      log.silly(
+        `C/renderApp: Resolved components`,
+        promisedComponents);
 
-    // Break down components into top-level window UI & context providers
-    const TopWindowComponent = promisedComponents[0].default;
-    var ctxProviderComponents = promisedComponents.
-      slice(1, promisedComponents.length).
-      map(item => item.default);
+      // Break down components into top-level window UI & context providers
+      const TopWindowComponent = promisedComponents[0].default;
+      var ctxProviderComponents = promisedComponents.
+        slice(1, promisedComponents.length).
+        map(item => item.default);
 
-    // Reorder context providers so that top-most is the most basic
-    ctxProviderComponents.reverse();
-    ctxProviderProps.reverse();
+      // Reorder context providers so that top-most is the most basic
+      ctxProviderComponents.reverse();
+      ctxProviderProps.reverse();
 
-    // Write out top-level window component JSX
-    var appMarkup = <TopWindowComponent query={searchParams} />;
+      // Write out top-level window component JSX
+      var appMarkup = <TopWindowComponent query={searchParams} />;
 
-    log.debug(
-      `C/renderApp: Got context provider components`,
-      ctxProviderComponents);
+      log.debug(
+        `C/renderApp: Got context provider components`,
+        ctxProviderComponents);
 
-    // Wrap the JSX into context provider components
-    for (const [idx, ContextProvider] of ctxProviderComponents.entries()) {
-      log.verbose(  
-        `C/renderApp: Initializing context provider #${idx}`,
-        ctxProviderComponents[idx],
-        ctxProviderProps[idx]);
+      // Wrap the JSX into context provider components
+      for (const [idx, ContextProvider] of ctxProviderComponents.entries()) {
+        log.verbose(  
+          `C/renderApp: Initializing context provider #${idx}`,
+          ctxProviderComponents[idx],
+          ctxProviderProps[idx]);
 
-      appMarkup = (
-        <ContextProvider {...ctxProviderProps[idx]}>
-          {appMarkup}
-        </ContextProvider>
-      );
-    }
+        appMarkup = (
+          <ContextProvider {...ctxProviderProps[idx]}>
+            {appMarkup}
+          </ContextProvider>
+        );
+      }
 
-    log.debug("C/renderApp: Rendering");
+      log.debug("C/renderApp: Rendering");
 
-    // Render the JSX
-    ReactDOM.render(appMarkup, appRoot);
+      // Render the JSX
+      ReactDOM.render(appMarkup, appRoot);
+    })();
+
+    return {
+      root: appRoot,
+      useData,
+    };
 
   } else {
     // Component specified in GET params is not present in app renderer config.
@@ -101,10 +141,9 @@ export const renderApp = async <A extends AppConfig, C extends RendererConfig<A>
     ReactDOM.render(<NonIdealState
       icon="error"
       title="Unknown component requested" />, appRoot);
-  }
 
-  return {
-    root: appRoot,
-  };
+    log.error("Unknown component requested", componentId);
+    throw new Error("Unknown component requested");
+  }
 
 };
