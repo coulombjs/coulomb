13746eb823976fa870f224c9fee74fcb9bd40a4e
Anton Strogonoff <anton@strogonoff.name>
2020-03-04 22:51:32 +0900

    gitpkg

---
+++ db/isogit-yaml/renderer/status.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { DatabaseStatusComponentProps } from '../../../config/renderer';
+import { BackendDescription, BackendStatus } from '../base';
+declare const BackendDetails: React.FC<DatabaseStatusComponentProps<BackendDescription, BackendStatus>>;
+export default BackendDetails;
---
+++ db/models.d.ts
@@ -0,0 +1,2 @@
+export declare type AnyIDType = string | number;
+export declare type Model = Record<string, any>;
