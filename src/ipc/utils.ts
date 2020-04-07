7c21bd94526b3ad05678653bf50c0985610a2d00
Anton Strogonoff <anton@strogonoff.name>
2020-04-08 04:16:31 +0900

    fix: Stop processing long numbers as dates

--- src/ipc/utils.ts
+++ src/ipc/utils.ts
@@ -2,6 +2,9 @@ import * as moment from 'moment';
 
 
 export function reviveJsonValue(key: string, val: any) {
+  if (!val || !val.hasOwnProperty('indexOf') || val.indexOf('-') < 0) {
+    return val;
+  }
   const timestamp = moment(val, moment.ISO_8601, true);
   if (timestamp.isValid()) {
     return timestamp.toDate();
