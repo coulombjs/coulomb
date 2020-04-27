48d5d7f01093153e16a4ae329e8a13ca6a3d1927
Anton Strogonoff <anton@strogonoff.name>
2020-04-27 16:44:50 +0900

    fix: Proper date value check in reviveJsonValue()

--- src/ipc/utils.ts
+++ src/ipc/utils.ts
@@ -2,7 +2,7 @@ import * as moment from 'moment';
 
 
 export function reviveJsonValue(key: string, val: any) {
-  if (!val || !val.hasOwnProperty('indexOf') || val.indexOf('-') < 0) {
+  if (!val || val.indexOf === undefined || val.indexOf('-') < 0) {
     return val;
   }
   const timestamp = moment(val, moment.ISO_8601, true);
