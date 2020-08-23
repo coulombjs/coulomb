a25c0fc716ac857f71d8f6213c33ea4ff55dafd0
Anton Strogonoff <anton@strogonoff.name>
2020-08-23 15:33:16 +0900

    fix(db/isogit): correctly check for matching remote URL

--- src/db/isogit-yaml/main/base.ts
+++ src/db/isogit-yaml/main/base.ts
@@ -211,8 +211,8 @@ class Backend extends VersionedFilesystemBackend {
       if (forceReset === true) {
         log.warn("C/db/isogit-yaml: Git is being force reinitialized");
         doInitialize = true;
-      } else if (!(await this.git.isUsingRemoteURLs({ origin: this.opts.repoURL }))) {
-        log.warn("C/db/isogit-yaml: Git has mismatching remote URL(s), reinitializing");
+      } else if (!(await this.git.isUsingRemoteURLs({ origin: `${this.opts.repoURL}.git` }))) {
+        log.warn("C/db/isogit-yaml: Git has mismatching remote URL, reinitializing with:", this.opts.repoURL);
         doInitialize = true;
       } else {
         log.info("C/db/isogit-yaml: Git is already initialized");
